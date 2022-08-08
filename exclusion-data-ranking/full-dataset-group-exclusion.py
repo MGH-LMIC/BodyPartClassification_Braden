3bd3ab9b578e514d75e2d0b2ef3061ca18dafda5
Braden Johnsen <55299028+b-johnsen@users.noreply.github.com>
2022-08-08 15:12:30 -0400

    organize w/folders and update exclusion code

--- exclusion-data-ranking/full-dataset-group-exclusion.py
+++ exclusion-data-ranking/full-dataset-group-exclusion.py
@@ -13,20 +13,19 @@ import time
 import os
 import copy
 
-cudnn.benchmark = True
-
-torch.manual_seed(17)
-
-
-class ImageFolderWithPaths(datasets.ImageFolder):
-    def __getitem__(self, index):
-        return super(ImageFolderWithPaths, self).__getitem__(index) + (self.imgs[index][0],) 
-
-
+class_size = 50
 image_size = 100
 
+#data_dir = '/Users/bradyjohnsen/Desktop/LMIC Internship/BodyPartProject/Datasets/'
+data_dir = '~/bodypartproject/'
+
+torch.use_deterministic_algorithms(True)
+torch.backends.cudnn.benchmark = False
+torch.manual_seed(17)
+np.random.seed(0)
+
 data_transforms = {
-    'Five-Training': transforms.Compose([
+    'full-training': transforms.Compose([
         transforms.Resize(image_size),
         transforms.CenterCrop(image_size),
         transforms.RandomHorizontalFlip(),
@@ -41,29 +40,28 @@ data_transforms = {
     ]),
 }
 
-#data_dir = '/Users/bradyjohnsen/Desktop/LMIC Internship/BodyPartProject/Datasets/'
-data_dir = '~/bodypartproject/'
+#def seed_worker(worker_id):
+    #worker_seed = torch.initial_seed() % 2**32
+    #np.random.seed(worker_seed)
+    #random.seed(worker_seed)
+
+g = torch.Generator()
+g.manual_seed(0)
 
 image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
                                           data_transforms[x])
-                  for x in ['Modified-Testing']}
+                  for x in ['full-training', 'Modified-Testing']}
 
-#training_data_dir = '/Users/bradyjohnsen/Desktop/LMIC Internship/BodyPartProject/Datasets/Five-Training'
-training_data_dir = '~/bodypartproject/Five-Training'
-
-image_datasets['Five-Training'] = ImageFolderWithPaths(
-    root=training_data_dir, transform=data_transforms['Five-Training'])
-
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
-                                              shuffle=True, num_workers=0)
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=32,
+                                              shuffle=True, num_workers=0, generator=g)
                for x in ['Modified-Testing']}
 
-dataloaders['Five-Training'] = torch.utils.data.DataLoader(image_datasets['Five-Training'], batch_size=100,
-                                              shuffle=True, num_workers=0)
+dataloaders['full-training'] = torch.utils.data.DataLoader(image_datasets['full-training'], batch_size=100,
+                                              shuffle=True, num_workers=0, generator=g)
 
 dataset_sizes = {x: len(image_datasets[x])
-                 for x in ['Five-Training', 'Modified-Testing']}
-class_names = image_datasets['Five-Training'].classes
+                 for x in ['full-training', 'Modified-Testing']}
+class_names = image_datasets['full-training'].classes
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -74,13 +72,16 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
 
+    acc_arr = np.zeros(num_epochs)
+    loss_arr = np.zeros(num_epochs)
+
     for epoch in range(num_epochs):
         print(f'Epoch {epoch}/{num_epochs - 1}')
         print('-' * 10)
 
         # Each epoch has a training and validation phase
-        for phase in ['Five-Training', 'Modified-Testing']:
-            if phase == 'Five-Training':
+        for phase in ['full-training', 'Modified-Testing']:
+            if phase == 'full-training':
                 model.train()  # Set model to training mode
             else:
                 model.eval()   # Set model to evaluate mode
@@ -89,8 +90,8 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
             running_corrects = 0
 
             # Iterate over data.
-            if phase == 'Five-Training':
-                for inputs, labels, paths in dataloaders[phase]:
+            if phase == 'full-training':
+                for inputs, labels in dataloaders[phase]:
                     inputs = inputs.to(device)
                     labels = labels.to(device)
 
@@ -99,20 +100,20 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
                     # forward
                     # track history if only in train
-                    with torch.set_grad_enabled(phase == 'Five-Training'):
+                    with torch.set_grad_enabled(phase == 'full-training'):
                         outputs = model(inputs)
                         _, preds = torch.max(outputs, 1)
                         loss = criterion(outputs, labels)
 
                         # backward + optimize only if in training phase
-                        if phase == 'Five-Training':
+                        if phase == 'full-training':
                             loss.backward()
                             optimizer.step()
 
                     # statistics
                     running_loss += loss.item() * inputs.size(0)
                     running_corrects += torch.sum(preds == labels.data)
-                if phase == 'Five-Training':
+                if phase == 'full-training':
                     scheduler.step()
             else:
                 for inputs, labels in dataloaders[phase]:
@@ -124,31 +125,34 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
                     # forward
                     # track history if only in train
-                    with torch.set_grad_enabled(phase == 'Five-Training'):
+                    with torch.set_grad_enabled(phase == 'full-training'):
                         outputs = model(inputs)
                         _, preds = torch.max(outputs, 1)
                         loss = criterion(outputs, labels)
 
                         # backward + optimize only if in training phase
-                        if phase == 'Five-Training':
+                        if phase == 'full-training':
                             loss.backward()
                             optimizer.step()
 
                     # statistics
                     running_loss += loss.item() * inputs.size(0)
                     running_corrects += torch.sum(preds == labels.data)
-                if phase == 'Five-Training':
+                if phase == 'full-training':
                     scheduler.step()
 
-            if phase == 'Five-Training':
-                epoch_loss = running_loss / (dataset_sizes[phase] - 1)
-                epoch_acc = running_corrects.double() / (dataset_sizes[phase] - 1)
+            if phase == 'full-training':
+                epoch_loss = running_loss / len(image_datasets[phase])
+                epoch_acc = running_corrects.double() / len(image_datasets[phase])
 
             else:
                 epoch_loss = running_loss / dataset_sizes[phase]
                 epoch_acc = running_corrects.double() / dataset_sizes[phase]
+                acc_arr[epoch] = epoch_acc
+                loss_arr[epoch] = epoch_loss
 
             print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
 
             # deep copy the model
             if phase == 'Modified-Testing' and epoch_acc > best_acc:
@@ -165,56 +169,63 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     model.load_state_dict(best_model_wts)
 
     print()
-
     print(f'Best val Acc: {best_acc:4f}')
-
-    add_grid = [global_pathlist[idx_exclu], best_acc.item()]
-
-    print(add_grid)
-
-    grid.append(add_grid)
-
-    torch.save(model_ft.state_dict(), f"{imagelist[idx_exclu]}-{best_acc:4f}.pth")
-
     print()
+    print(f"Accuracy: {acc_arr}")
+    print()
+    print(f"Testing Loss: {loss_arr}")
     print()
 
-    return model
+    return model, best_acc
 
+grid = []
 
-rows, cols = (dataset_sizes['Five-Training'], 2)
-grid = [[0]*cols]*rows
+print(f"Dataset size: {dataset_sizes['full-training']}")
+print()
 
-print(f"Dataset size: {dataset_sizes['Five-Training']}")
-
-for inputs, labels, paths in dataloaders['Five-Training']:
-   tlabels = labels
-   tpaths = paths 
-
-global_labels = labels
-global_pathlist = tpaths
-
+global_labels = []
+global_pathlist = []
 imagelist = []
 
-for idx in range(len(global_pathlist)):
-    imagelist.append(global_pathlist[idx].rsplit('/', 1)[-1])
+for i in range(dataset_sizes['full-training']):
+    fname, label = image_datasets['full-training'].samples[i]
+    global_pathlist.append(fname)
+    global_labels.append(label)
+    imagelist.append(global_pathlist[i].rsplit('/', 1)[-1])
 
-print(imagelist)
+#print(imagelist)
+print()
 
-for idx_exclu in range(dataset_sizes['Five-Training']):
+idx_start = []
+for r in range(len(class_names)):
+    for x in range(dataset_sizes['full-training']):
+        if global_labels[x] == r:
+            idx_start.append(x)
+            break
 
-    image_datasets['Five-Training'] = ImageFolderWithPaths(
-    root=training_data_dir, transform=data_transforms['Five-Training'])
+for group_num in range(dataset_sizes['full-training']//(class_size*len(class_names))):
 
-    image_datasets['Five-Training'].imgs.remove((global_pathlist[idx_exclu], global_labels[idx_exclu]))
+    print(f"Group number: {group_num}")
+    print()
+    print(f"Start indexes: {idx_start}")
+    print()
+
+    image_datasets['full-training'] = datasets.ImageFolder(os.path.join(data_dir, 'full-training'), transform=data_transforms['full-training'])
+
+    #add_grid = ['',] * (class_size*len(class_names))
+    add_grid = []
+
+    for n in range(len(class_names)):
+        for i in range(class_size):
+            image_datasets['full-training'].imgs.remove((global_pathlist[idx_start[n]], global_labels[idx_start[n]]))
+            print(f"Removed \"{imagelist[idx_start[n]]}\"")
+            add_grid.append(imagelist[idx_start[n]])
+            idx_start[n] = idx_start[n] + 1
 
     print()
-    print(f'Excluding: {global_pathlist[idx_exclu]} of class {class_names[global_labels[idx_exclu]]}')
 
-    dataloaders['Five-Training'] = torch.utils.data.DataLoader(image_datasets['Five-Training'], batch_size=100,
-                                              shuffle=True, num_workers=0)
-
-    print()
+    dataloaders['full-training'] = torch.utils.data.DataLoader(image_datasets['full-training'], batch_size=50,
+                                              shuffle=True, num_workers=0, generator=g)
 
     model_ft = models.resnet18(pretrained=True)
     num_ftrs = model_ft.fc.in_features
@@ -232,7 +243,22 @@ for idx_exclu in range(dataset_sizes['Five-Training']):
     exp_lr_scheduler = lr_scheduler.StepLR(
         optimizer_ft, step_size=7, gamma=0.1)
 
-    model_ft = train_model(model_ft, criterion, optimizer_ft,
-                           exp_lr_scheduler, num_epochs=5)
+    model_ft, best_accuracy = train_model(model_ft, criterion, optimizer_ft,
+                           exp_lr_scheduler, num_epochs=10)
+
+    torch.save(model_ft.state_dict(), f"{group_num}-{best_accuracy:4f}.pth")
+
+    add_grid.append(best_accuracy.item())
+
+    grid.append(add_grid)
+    print(add_grid)
+
+    print()
+    print()
 
 print(grid)
+
+np.savetxt("grid.csv", 
+           grid,
+           delimiter =", ", 
+           fmt ='% s')
